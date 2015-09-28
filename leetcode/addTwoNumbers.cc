52e242214cbec68902d7454035dd44eb59f690bb
Aaron Zhang <aaron.cheung@hotmail.com>
2015-09-28 17:39:09 +0800

    add leetcode solutions.

---
+++ leetcode/addTwoNumbers.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode{
+	int val;
+	ListNode *next;
+	ListNode(int x): val(x), next(NULL){}
+};
+
+void printList(ListNode *x){
+	while(x != NULL){
+		cout<<x<<"->";
+		x = x->next;
+	}
+	cout<<endl;
+}
+
+class Solution{
+public:
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2){
+		int x, y, carry = 0, sum = 0;
+		ListNode *h = NULL, **t = &h;
+
+		while(l1 != NULL || l2 != NULL){
+			x = getValueAndMoveNext(l1);
+			y = getValueAndMoveNext(l2);
+
+			sum = carry + x + y;
+
+			ListNode *node = new ListNode(sum % 10);
+			*t = node;
+			t = (&node->next);
+
+			carry = sum / 10;
+		}
+
+		if(carry > 0){
+			ListNode *node = new ListNode(carry % 10);
+			*t = node;
+		}
+
+		return h;
+	}
+
+private:
+	int getValueAndMoveNext(ListNode* &l){
+		int x = 0;
+		if(l != NULL){
+			x = l->val;
+			l = l->next;
+		}
+		return x;
+	}
+};
+
+int main(){
+	return 1;
+}
---
+++ leetcode/longestSubstringWithoutRepeatingCharacters.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int lengthOfLongestSubstring(string s){
+	map<char, int> m;
+	int maxLen = 0, lastPosition = -1;
+	for(int i = 0; i < s.size(); i++){
+		if(m.find(s[i]) != m.end() && lastPosition < m[s[i]]){
+			lastPosition = m[s[i]];
+		}
+		if(i - lastPosition > maxLen){
+			maxLen = i - lastPosition;
+		}
+		m[s[i]] = i;
+	}
+	return maxLen;
+}
+
+void main(){
+	cout<<lengthOfLongestSubstring("abcabcd")<<endl;
+	cout<<lengthOfLongestSubstring("aaaa")<<endl;
+}
