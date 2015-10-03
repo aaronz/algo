3c2f6d5bd3049780e6f5b356b05eba3d0c2b5954
Aaron Zhang <aaron.cheung@hotmail.com>
2015-10-03 16:51:42 +0800

    format code.

--- leetcode/longestSubstringWithoutRepeatingCharacters.cc
+++ leetcode/longestSubstringWithoutRepeatingCharacters.cc
@@ -5,21 +5,21 @@
 using namespace std;
 
 int lengthOfLongestSubstring(string s){
-	map<char, int> m;
-	int maxLen = 0, lastPosition = -1;
-	for(int i = 0; i < s.size(); i++){
-		if(m.find(s[i]) != m.end() && lastPosition < m[s[i]]){
-			lastPosition = m[s[i]];
-		}
-		if(i - lastPosition > maxLen){
-			maxLen = i - lastPosition;
-		}
-		m[s[i]] = i;
-	}
-	return maxLen;
+    map<char, int> m;
+    int maxLen = 0, lastPosition = -1;
+    for (int i = 0; i < s.size(); i++){
+        if (m.find(s[i]) != m.end() && lastPosition < m[s[i]]){
+            lastPosition = m[s[i]];
+        }
+        if (i - lastPosition > maxLen){
+            maxLen = i - lastPosition;
+        }
+        m[s[i]] = i;
+    }
+    return maxLen;
 }
 
 void main(){
-	cout<<lengthOfLongestSubstring("abcabcd")<<endl;
-	cout<<lengthOfLongestSubstring("aaaa")<<endl;
+    cout << lengthOfLongestSubstring("abcabcd") << endl;
+    cout << lengthOfLongestSubstring("aaaa") << endl;
 }
